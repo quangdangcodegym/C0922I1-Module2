bfe82cdf07a69e171152340ddbd9f0e2577cf202
quangdangcodegym <quang.dang@codegym.vn>
2022-11-11 15:10:28 +0700

    java mang 2 chieu

--- hellojava/src/main/java/com/codegym/Main.java
+++ hellojava/src/main/java/com/codegym/Main.java
@@ -1,14 +1,203 @@
 package com.codegym;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
+    public static int max = 10;
+    public static int min  = 1;
 
     public static void main(String[] args) {
 
+        int a;
+//        double[] numbers = {5.0, 7, 1, 7};
+//        double[] numbers1 = new double[5];
+//        double [] numbers2;
 
+        // Tìm tìm số 5 có nằm trong mảng hay không
+        System.out.println("Nhap so dòng: ");
+        int row = Integer.parseInt(scanner.nextLine());
+        System.out.println("Nhap so cột: ");
+        int col = Integer.parseInt(scanner.nextLine());
+        int [][] arr = createArrayTwoWay(row, col);
+        System.out.println(printArrayTwoWay(arr));
+
+
+
+
+    }
+
+    public static String printArrayTwoWay(int[][] arr) {
+        String str = "";
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                str += arr[i][j];
+                if (j != arr[i].length - 1) {
+                    str += ",";
+                }
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    public static int[][] createArrayTwoWay(int nRow, int nCol) {
+        int [][] arr = new int[nRow][nCol];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) ((Math.random() * (max - min)) + min);
+            }
+        }
+        return arr;
+    }
+
+    public static void reverseArrayView() {
+        System.out.println("Nhap so luong phan tu cua mang");
+        int number = scanner.nextInt();
+        int [] arr = createRandomArray(number);
+        System.out.println(printArray(arr));
+
+//        int [] arr1 = reverseArray1(arr);
+        reverseArray(arr);
+
+        System.out.println(printArray(arr));
+    }
+    public static int[] reverseArray1(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        return arr;
+    }
+    public static void reverseArray(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+    }
+    public static void findIndexView() {
+        System.out.println("Nhap so luong phan tu cua mang");
+        int number = scanner.nextInt();
+        int [] arr = createRandomArray(number);
+        System.out.println(printArray(arr));
+
+
+        System.out.println("Tìm gia tri tron mảng trả về vị trí");
+        System.out.println("Nhập giá tri cần tìm: ");
+        int value = scanner.nextInt();
+        System.out.println(findIndex(arr, value));
+    }
+    public static void createArrayView() {
+        System.out.println("Nhap so luong phan tu cua mang");
+        int number = scanner.nextInt();
+        int [] arr = createRandomArray(number);
+        System.out.println(printArray(arr));
+    }
+
+
+    public static int findIndex(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static String printArray(int[] arr) {
+        String str = "[";
+        for (int i = 0; i < arr.length; i++) {
+            str += arr[i] ;
+            if (i == arr.length - 1) {
+                str += "]";
+            }else{
+                str +=  ",";
+            }
+        }
+        return str;
+
+    }
+    public static int[] createRandomArray(int size) {
+        int[] numbers = new int[size];
+        for (int i = 0; i< numbers.length; i++) {
+            numbers[i] =  (int) ((Math.random() * (max - min)) + min);
+        }
+        return numbers;
+    }
+
+    public static void totalPrimeView() {
+        System.out.printf("Tỉnh tống %d só nguyên tố đầu tiên " + 5);
+        int numberMax = scanner.nextInt();
+        int sum = 0;
+        int count = 0;
+        int i = 2;
+        while (i < numberMax) {
+            if(checkPrimeNumber(i)){
+                sum += i;
+                System.out.println(i);
+                count++;
+            }
+            if (count == 5) {
+                break;
+            }
+            i++;
+        }
+        System.out.println("Sum: " + sum);
+    }
+    public static void printPrimeLessThanNumberView() {
+        System.out.println("Nhap vao so: ");
+        int number = scanner.nextInt();
+        // [2,3,5,7]
+        int i = 2;
+        String str = "";
+        for (; i < number; i++) {
+            if (checkPrimeNumber(i)) {
+                str += i + ",";
+            }
+        }
+        // str = 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,
+        System.out.println(str.substring(0,str.length()-1));
+        // O a d o
+        System.out.println("oa".compareTo("Oa"));;
+
+    }
+    public static boolean checkPrimeNumber(int number) {
+        boolean flag = true;
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+
+    }
+    public static void checkPrimiter1() {
+        int number = scanner.nextInt();
+        // Kiểm tra nguyen to
+        // number = 15
+        if (number < 2) {
+            System.out.println(number + " Khong phai la so nguyen to");
+        }else {
+            if (number == 2) {
+                System.out.printf("%d la so nguyen to", number);
+            }
+        }
+        // 97
+        for(int i = 2 ; i<number; i++){
+            if (number % i == 0) {
+                System.out.println(number + " Khong phai la so nguyen to");
+                break;
+            }
+            if (i == number - 1) {
+                System.out.printf("%d la so nguyen to", number);
+            }
+        }
     }
     // Bài 1
     public static void getMonth() {
