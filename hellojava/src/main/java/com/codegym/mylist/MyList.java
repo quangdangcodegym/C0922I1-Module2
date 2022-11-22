9c1bbef0b8039e40517b01a4ad73a1b6716b0351
quangdangcodegym <quang.dang@codegym.vn>
2022-11-22 11:51:58 +0700

    my list

--- hellojava/src/main/java/com/codegym/mylist/MyList.java
+++ hellojava/src/main/java/com/codegym/mylist/MyList.java
@@ -1,6 +1,9 @@
 package com.codegym.mylist;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
 
 public class MyList<E> {
     private int size;
@@ -12,6 +15,11 @@ public class MyList<E> {
         size = 0;
     }
 
+    public MyList(int lenght) {
+        elements = new Object[lenght];
+        size = 0;
+    }
+
     public void add(E e) {
         if (size >= DEFAUTL_SIZE) {
             ensureCapa();
@@ -19,17 +27,91 @@ public class MyList<E> {
         elements[size] = e;
         size++;
     }
+    public void remove (int index){
+        if(index>=size|| index<0) throw new IndexOutOfBoundsException();
+        for (int i = index; i < size; i ++){
+            if(i<size-1){
+                this.elements[i]=this.elements[i+1];
+            }
+        }
+        size--;
+    }
+
+    public E get(int index) {
+        if(index>=size|| index<0) throw new IndexOutOfBoundsException();
+        return (E) this.elements[index];
+    }
 
     private void ensureCapa() {
         elements = Arrays.copyOf(elements, DEFAUTL_SIZE*2);
     }
 
+    public int indexOf(E e) {
+        for (int i = 0; i < size; i++)
+            if (e.equals(elements[i])) return i;
+        return -1;
+    }
+    @Override
+    public String toString() {
+        /**
+        StringBuilder arrString = new StringBuilder();
+        arrString.append("[");
+        for(int i =0; i<size;i++){
+            if(i==size-1) {
+                arrString.append(this.elements[i]).append("]");
+                break;
+            };
+            arrString.append(this.elements[i]).append(",");
+        }
+         **/
+
+        String arrString = "[";
+        for(int i =0; i<size;i++){
+            if(i==size-1) {
+                arrString += this.elements[i] + "]";
+                break;
+            };
+            arrString += this.elements[i] + ",";
+        }
+        return "MyList{" +
+                "elements=" + arrString +
+                '}';
+    }
+    public MyList clone() {
+        MyList<E> clone = new MyList<E>(this.elements.length);
+        for (E e : (E[]) elements) {
+            clone.add(e);
+        }
+        return clone;
+    }
+
     public static void main(String[] args) {
-        MyList<Integer> objs = new MyList<>();
-        objs.add(3);
-        objs.add(1);
-        objs.add(3);
-        objs.add(1);
-        objs.add(7);
+//        MyList<Integer> objs = new MyList<>();
+//        objs.add(1);
+//        objs.add(3);
+//        objs.add(1);
+//        objs.add(7);
+//        System.out.println(objs);
+//        System.out.println("After deleted");
+//        objs.remove(6);
+//        System.out.println(objs);
+//
+//
+//        List<String> c;
+
+        Integer c = 5;
+        Integer d = 5;
+
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ hellojava/src/main/java/com/codegym/mylist/Shape.java
@@ -0,0 +1,18 @@
+package com.codegym.mylist;
+
+public class Shape {
+
+    private int radius;
+    public Shape(){};
+    public Shape(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int color) {
+        this.radius = color;
+    }
+}
