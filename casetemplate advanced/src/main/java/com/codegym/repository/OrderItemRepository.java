c2f13d51be9af6475a6e9eb2ea8cb151a9d306f8
quangdangcodegym <quang.dang@codegym.vn>
2022-12-18 14:05:52 +0700

    update repository

--- casetemplate advanced/src/main/java/com/codegym/repository/OrderItemRepository.java
+++ casetemplate advanced/src/main/java/com/codegym/repository/OrderItemRepository.java
@@ -13,16 +13,11 @@ public class OrderItemRepository extends DatabaseContext<OrderItem> {
 
         path = "./data/orderitem.txt";
     }
-//    @Override
-//    public List<OrderItem> getAll() {
-//        List<OrderItem> orderItems = fileService.readData(path, EModel.ORDERITEM);
-//        return orderItems;
-//    }
 
     public void createOrderItem(List<OrderItem> orderItem) {
-//        List<OrderItem> allOrderItems = getAll();
-//        allOrderItems.addAll(orderItem);
-//
-//        addList(allOrderItems);
+        List<OrderItem> allOrderItems = getAll();
+        allOrderItems.addAll(orderItem);
+
+        addList(allOrderItems);
     }
 }
